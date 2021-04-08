3379f80dc8a45735e0109e125957a536d6480d09
iy06 <yuki.inoue@di-v.co.jp>
2021-04-08 17:01:20 +0900

    homeの要素を作成

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,6 +3,25 @@ import './index.scss';
 
 export const Home = () => {
   return (
-    <h1>Hello World</h1>
+    <div className='wrapper'>
+      {/* ヘッダーコンポーネント */}
+      <header className='header'>DigSong</header>
+      <div className='main'>
+        {/* ソングコンポーネント */}
+        <div className='song'>
+          <div className='song__data'>
+            <p className='key'>#Cmaj</p>
+            <p className='bpm'>128</p>
+          </div>
+          <img className='song__image' src='logo512.png'/>
+          <h3 className='song__title'>AllOrNothing</h3>
+          <audio className='song__bar' controls controlsList='nodownload' preload='meatadata'>
+            <source src='AllOrNothing.mp3'/>
+          </audio>
+        </div>
+      </div>
+      {/* フッターコンポーネント */}
+      <footer>Footer</footer>
+    </div>
   )
 };
