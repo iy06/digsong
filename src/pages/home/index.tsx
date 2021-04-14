f6d347b63b744c47519a59a9b2d730530b098bd5
iy06 <yuki.inoue@di-v.co.jp>
2021-04-14 14:26:47 +0900

    useEffectをつかってapi通信をする関数を呼び出す記述を追加

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './style.scss';
 import { Header } from '../../components/header';
 import { Footer } from '../../components/footer';
 import { SongSlider } from '../../components/songslider';
+import { songRequest } from '../../requests/songRequest'; // axiosのrequestファイル
 
 export const Home = () => {
+  // 初回ロード時に実行するuseEffect
+  useEffect (() => {
+    const showSongs = async () => {
+      const response = await songRequest( 'fetchSongs' );
+      console.log(response);
+    }
+    showSongs();
+  }, []);
+
   return (
     <div className='wrapper'>
       {/* Headerコンポーネント */}
