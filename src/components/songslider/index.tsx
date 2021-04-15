01a98924ab034a60e06e9657c2e7b2d70982afe9
iy06 <yuki.inoue@di-v.co.jp>
2021-04-15 15:46:10 +0900

    songコンポーネントにpropsを渡すよう修正

--- src/components/songslider/index.tsx
+++ src/components/songslider/index.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Song } from '../song';
+import { SongType } from '../../interfaces/SongType';
 import './style.scss';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
-export const SongSlider = () => {
+interface Props {
+  songs: SongType[];
+};
+
+export const SongSlider = ( props: Props ) => {
   const settings = {
     dots:           true,
     infinite:       true,
@@ -16,10 +21,10 @@ export const SongSlider = () => {
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 1,
+          slidesToShow:   1,
           slidesToScroll: 1,
-          infinite: true,
-          dots: true
+          infinite:       true,
+          dots:           true
         }
       },
     ]
@@ -28,12 +33,9 @@ export const SongSlider = () => {
   return (
     <Slider { ...settings }>
       {/* Songコンポーネント */}
-      <Song />
-      <Song />
-      <Song />
-      <Song />
-      <Song />
-      <Song />
+      { props.songs.map(( song: SongType ) => {
+        return <Song song={ song }/>
+      })}
     </Slider>
   );
 };
