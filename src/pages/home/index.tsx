7430966a1f7c0753d8e652f22b94c6e98fc660e5
iy06 <yuki.inoue@di-v.co.jp>
2021-04-15 15:47:18 +0900

    useEffectを使ってapiからfetchしてくるよう修正

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,26 +1,32 @@
 import React, { useEffect, useState } from 'react';
-import './style.scss';
 import { Header } from '../../components/header';
 import { Footer } from '../../components/footer';
 import { SongSlider } from '../../components/songslider';
 import { songRequest } from '../../requests/songRequest'; // axiosのrequestファイル
+import { useDataReducer } from '../../hooks/useDataReducer'; // reducerのファイル
+import './style.scss';
 
 export const Home = () => {
+  const [ data, dispatch ] = useDataReducer();
   // 初回ロード時に実行するuseEffect
   useEffect (() => {
-    const showSongs = async () => {
-      const response = await songRequest( 'fetchSongs' );
-      console.log(response);
-    }
-    showSongs();
+    const fetchData = async () => {
+      const songs = await songRequest( 'fetchSongs' );
+      dispatch({ type: 'songsUpdate', payload: { song: songs } });
+    };
+    fetchData();
   }, []);
+  // dataが変わった時のuseEffect
+  useEffect(() => {
+    console.log( data );
+  }, [ data ]);
 
   return (
     <div className='wrapper'>
       {/* Headerコンポーネント */}
       <Header />
       <div className='main'>
-        <SongSlider />
+        <SongSlider songs={ data.songsData }/>
       </div>
       {/* Footerコンポーネント */}
       <Footer />
