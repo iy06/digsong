145b99f4b12f2af0e243a6f0cc11c99c029f655a
iy06 <yuki.inoue@di-v.co.jp>
2021-04-18 19:04:58 +0900

    api側にparameterを送信できるリクエストを追加

--- src/requests/songRequest.ts
+++ src/requests/songRequest.ts
@@ -1,22 +1,31 @@
 import axiosBase from 'axios';
+import { SongType } from '../interfaces/SongType';
 
 type action = 'fetchSongs' | 'createSongs';
+type parameter = { data: SongType };
 
 const api = axiosBase.create({
   baseURL:      'http://localhost:3001/api',
   responseType: 'json',
 });
 
-export const songRequest: ( action: action ) => any = async ( action: action ) => {
-  switch ( action ) {
-    case 'fetchSongs':
-      const fetchSongs = await api.get( '/songs' );
-      console.log(fetchSongs.data);
-      return fetchSongs.data;
-    case 'createSongs':
-      const createSongs = await api.post( '/songs', 'createアクションへのリクエスト' );
-      return createSongs.data;
-    default:
-      return null;
-  };
+export const songRequest: ( action: action, parameter?: parameter ) => any = async ( action: action, parameter?: parameter ) => {
+  if ( parameter ) {
+    switch ( action ) {
+      case 'createSongs':
+        const createSongs = await api.post( '/songs', parameter.data );
+        return createSongs.data;
+      default:
+        return null;
+    };
+  } else {
+    switch ( action ) {
+      case 'fetchSongs':
+        const fetchSongs = await api.get( '/songs' );
+        console.log(fetchSongs.data);
+        return fetchSongs.data;
+      default:
+        return null;
+    };
+  }
 };
