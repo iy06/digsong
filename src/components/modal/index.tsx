b530170a9c06abf432e53048b5761897786d3e8d
iy06 <yuki.inoue@di-v.co.jp>
2021-04-18 19:00:25 +0900

    valueを状態管理するStateと関数を追加

--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
+import { DataContext } from '../../pages/home';
+import { SongType } from '../../interfaces/SongType';
+import { songRequest } from '../../requests/songRequest';
 import Modal from 'react-modal';
-// import { SongType } from '../../interfaces/SongType';
-// import { songRequest } from '../../requests/songRequest';
 import CropOriginalIcon from '@material-ui/icons/CropOriginal';
 import MusicVideoIcon from '@material-ui/icons/MusicVideo';
 import './style.scss';
-
 interface Props {
   isOpen:      boolean;
   handleClose: () => void;
@@ -35,6 +35,51 @@ const addFileName = ( event: any ) => {
 
 export const FormModal = ( props: Props ) => {
   Modal.setAppElement( '#root' );
+  const { dispatch } = useContext( DataContext );
+  const [ title, setTitle ] = useState<string>( '' );
+  const [ key, setKey ] = useState<string>( '' );
+  const [ bpm, setBpm ] = useState<string>( '' );
+  const [ image, setImage ] = useState<any>( '' );
+  const [ song_data, setSongData ] = useState<any>( '' );
+
+  const handleChangeTitle = ( event: React.ChangeEvent<HTMLInputElement> ) => {
+    setTitle( event.target.value );
+  };
+  const handleChangeKey = ( event: React.ChangeEvent<HTMLInputElement> ) => {
+    setKey( event.target.value );
+  };
+  const handleChangeBpm = ( event: React.ChangeEvent<HTMLInputElement> ) => {
+    setBpm( event.target.value );
+  };
+  const handleChangeImage = ( event: React.ChangeEvent<HTMLInputElement> ) => {
+    console.log(event.target.value);
+    // if ( event.target && event.target.files) {
+      setImage( event.target.value );
+    // }
+  };
+  const handleChangeSongData = ( event: React.ChangeEvent<HTMLInputElement> ) => {
+    // if ( event.target && event.target.files) {
+      setSongData( event.target.value );
+    // }
+  };
+  const onClickSubmit = async () => {
+    const newData = {
+      id:        0,
+      title:     title,
+      key:       key,
+      bpm:       bpm,
+      image:     image,
+      song_data: song_data,
+    };
+
+    try {
+      const songs: SongType[] = await songRequest( 'createSongs', { data: newData } );
+      dispatch( { type: 'songsUpdate', payload: { songs: songs } } )
+    } catch ( error ) {
+      console.log( error.message );
+    };
+    props.handleClose();
+  };
 
   return (
     <Modal
@@ -47,24 +92,24 @@ export const FormModal = ( props: Props ) => {
         <div className='post-form__file-box'>
           <label htmlFor="file-image">
             <CropOriginalIcon />
-            <input onChange={ addFileName } id='file-image' type='file'/>
+            <input onChange={ (event) => { addFileName(event); handleChangeImage(event); } } value={ image } name='image' id='file-image' type='file'/>
             <p id='file-name' />
           </label>
           <label htmlFor="file-song">
             <MusicVideoIcon />
-            <input onChange={ addFileName } id='file-song' type='file'/>
+            <input onChange={ (event) => { addFileName(event); handleChangeSongData(event); } } value={ song_data } name='song_data' id='file-song' type='file'/>
             <p id='file-name' />
           </label>
         </div>
         <div className='post-form__input-box'>
           <h4>Title</h4>
-          <input className='input-title' type='text' placeholder='Sample Music'/>
+          <input onChange={ handleChangeTitle } value={ title } name={ title } className='input-title' type='text' placeholder='Sample Music'/>
           <h4>Key</h4>
-          <input className='input-key' type='text' placeholder='Cmaj'/>
+          <input onChange={ handleChangeKey } value={ key } name={ key } className='input-key' type='text' placeholder='Cmaj'/>
           <h4>Bpm</h4>
-          <input className='input-bpm' type='text' placeholder='128bpm'/>
+          <input onChange={ handleChangeBpm } value={ bpm } name={ bpm } className='input-bpm' type='text' placeholder='128bpm'/>
         </div>
-        <input className='post-form__submit' type='button' value='▶'/>
+        <input onClick={ onClickSubmit } className='post-form__submit' type='button' value='▶'/>
       </form>
     </Modal>
   );
