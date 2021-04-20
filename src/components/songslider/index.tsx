82ee3d4e6e0aa8915952be310ca971472b14f888
iy06 <yuki.inoue@di-v.co.jp>
2021-04-20 17:51:00 +0900

    React-slickの削除するための修正

--- src/components/songslider/index.tsx
+++ src/components/songslider/index.tsx
@@ -3,37 +3,16 @@ import { DataContext } from '../../pages/home';
 import { Song } from '../song';
 import { SongType } from '../../interfaces/SongType';
 import './style.scss';
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
 
 export const SongSlider = () => {
   const { data } = useContext( DataContext );
-  const settings = {
-    dots:           true,
-    infinite:       true,
-    slidesToShow:   1,
-    slidesToScroll: 1,
-    speed:          500,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow:   1,
-          slidesToScroll: 1,
-          infinite:       true,
-          dots:           true
-        }
-      },
-    ]
-  };
 
   return (
-    <Slider { ...settings }>
+    <div className='song-slider'>
       {/* Songコンポーネント */}
       { data.songsData.map(( song: SongType ) => {
         return <Song song={ song } key={ song.id }/>
       })}
-    </Slider>
+    </div>
   );
 };
