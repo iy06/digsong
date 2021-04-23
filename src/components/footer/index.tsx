fff79545a84f7f101e8aa331ca1ef358d8fc6307
iy06 <yuki.inoue@di-v.co.jp>
2021-04-23 19:03:38 +0900

    file名の大文字小文字を差分に反映させる

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './style.scss';
+import GitHubIcon from '@material-ui/icons/GitHub';
+
+
+export const Footer = () => {
+  return (
+    <footer className='footer'>
+      <span className='footer__github'>
+        <a href='https://github.com/iy06/digsong'>
+          <GitHubIcon />
+        </a>
+      </span>
+      <span className='footer__author'>©︎iy06</span>
+    </footer>
+  );
+};
