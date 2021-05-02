9d219c6a1919da9a3d27bc9cd175bcf4abafdf76
iy06 <yuki.inoue@di-v.co.jp>
2021-05-02 15:52:41 +0900

    Headerコンポーネントのコードを統一

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,10 +1,6 @@
-import React from 'react';
-import './style.scss';
+import React from "react";
+import "./style.scss";
 
 export const Header = () => {
-  return (
-    <header className='header'>
-      DigSong
-    </header>
-  );
+  return <header className="header">DigSong</header>;
 };
