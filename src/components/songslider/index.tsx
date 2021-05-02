7ebd20d6a607e7092da620b21c0bd2bdfcd22d46
iy06 <yuki.inoue@di-v.co.jp>
2021-05-02 14:09:32 +0900

    songSliderコンポーネントのコードを統一

--- src/components/songslider/index.tsx
+++ src/components/songslider/index.tsx
@@ -1,22 +1,30 @@
-import React, { useContext } from 'react';
-import { DataContext } from '../../pages/home';
-import { Song } from '../song';
-import { SongType } from '../../interfaces/SongType';
-import './style.scss';
+import React, { useContext } from "react";
+import { DataContext } from "../../pages/home";
+import { Song } from "../song";
+import { SongType } from "../../interfaces/SongType";
+
+import "./style.scss";
 
 interface Props {
   handleOpen: () => void;
-  getSong: (id: number) => void;
+  getSelectSong: (id: number) => void;
 };
 
 export const SongSlider = (props: Props) => {
-  const { data } = useContext( DataContext );
+  const { data } = useContext(DataContext);
 
   return (
-    <div className='song-slider'>
+    <div className="song-slider">
       {/* Songコンポーネント */}
-      { data.songsData.map(( song: SongType ) => {
-        return <Song song={ song } key={ song.id } handleOpen={ props.handleOpen } getSong={ props.getSong }/>
+      {data.songsData.map((song: SongType) => {
+        return (
+          <Song
+            song={song}
+            key={song.id}
+            handleOpen={props.handleOpen}
+            getSelectSong={props.getSelectSong}
+          />
+        );
       })}
     </div>
   );
