47bb0df6189087ffe5ccd3f81abc09bb7cfa80d2
iy06 <yuki.inoue@di-v.co.jp>
2021-05-02 15:52:03 +0900

    Footerコンポーネントのコードを統一

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,15 +1,14 @@
-import React from 'react';
-import './style.scss';
-import GitHubIcon from '@material-ui/icons/GitHub';
-
+import React from "react";
+import "./style.scss";
+import GitHubIcon from "@material-ui/icons/GitHub";
 
 export const Footer = () => {
   return (
-    <footer className='footer'>
-      <span className='footer__github'>
-        <a href='https://github.com/iy06/digsong'>
+    <footer className="footer">
+      <span className="footer__github">
+        <a href="https://github.com/iy06/digsong">
           <GitHubIcon />
-          <span className='author'>©︎iy06</span>
+          <span className="author">©︎iy06</span>
         </a>
       </span>
     </footer>
